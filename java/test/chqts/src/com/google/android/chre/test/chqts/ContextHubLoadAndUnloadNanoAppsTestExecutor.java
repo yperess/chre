6a17d0dbf617799761aba332c0e840ffdc2a0d68
Lei Ju <leiju@google.com>
2022-06-03 16:33:03 -0700

    Create an executor for LoadUnloadNanoappTest.
    Test: Manually run the test and pass.
    Bug: 151159967
    
    Change-Id: I412c29b6b2e808770d0e24d82e670bce9e9b2a47

---
+++ java/test/chqts/src/com/google/android/chre/test/chqts/ContextHubLoadAndUnloadNanoAppsTestExecutor.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.chre.test.chqts;
+
+import static com.google.android.utils.chre.ContextHubServiceTestHelper.TIMEOUT_SECONDS_LOAD;
+import static com.google.android.utils.chre.ContextHubServiceTestHelper.TIMEOUT_SECONDS_UNLOAD;
+import static com.google.common.truth.Truth.assertWithMessage;
+
+import android.hardware.location.ContextHubInfo;
+import android.hardware.location.ContextHubManager;
+import android.hardware.location.ContextHubTransaction;
+import android.hardware.location.NanoAppBinary;
+
+import com.google.android.utils.chre.ContextHubServiceTestHelper;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class ContextHubLoadAndUnloadNanoAppsTestExecutor {
+    private static final int NUM_TEST_CYCLES = 10;
+
+    private final NanoAppBinary mNanoAppBinary;
+    private final List<Long> mNanoAppIdList;
+    private final ContextHubServiceTestHelper mTestHelper;
+
+    private static class OnLoadUnloadCompleteListener
+            implements ContextHubTransaction.OnCompleteListener<Void> {
+        private final CountDownLatch mCountDownLatch = new CountDownLatch(1);
+
+        @Override
+        public void onComplete(
+                ContextHubTransaction<Void> transaction,
+                ContextHubTransaction.Response<Void> response) {
+            int result = response.getResult();
+            String type =
+                    ContextHubTransaction.typeToString(transaction.getType(), true /* upperCase */);
+            assertWithMessage("%s transaction failed with error code %s", type, result)
+                    .that(result)
+                    .isEqualTo(ContextHubTransaction.RESULT_SUCCESS);
+            mCountDownLatch.countDown();
+        }
+
+        CountDownLatch getCountDownLatch() {
+            return mCountDownLatch;
+        }
+    }
+
+    public ContextHubLoadAndUnloadNanoAppsTestExecutor(
+            ContextHubManager contextHubManager,
+            ContextHubInfo contextHubInfo,
+            NanoAppBinary nanoAppBinary) {
+        mTestHelper = new ContextHubServiceTestHelper(contextHubInfo, contextHubManager);
+        mNanoAppBinary = nanoAppBinary;
+        mNanoAppIdList = Collections.singletonList(mNanoAppBinary.getNanoAppId());
+    }
+
+    public void init() throws Exception {
+        mTestHelper.initAndUnloadAllNanoApps();
+    }
+
+    public void deinit() {
+        mTestHelper.deinit();
+    }
+
+    /**
+     * Repeatedly loads and unloads a nanoapp synchronously, and verifies that the naonapp is loaded
+     * successfully.
+     */
+    public void loadUnloadSyncTest() throws Exception {
+        for (int i = 0; i < NUM_TEST_CYCLES; i++) {
+            mTestHelper.loadNanoAppAssertSuccess(mNanoAppBinary);
+            mTestHelper.assertNanoAppsLoaded(mNanoAppIdList);
+
+            mTestHelper.unloadNanoAppAssertSuccess(mNanoAppBinary.getNanoAppId());
+            mTestHelper.assertNanoAppsNotLoaded(mNanoAppIdList);
+        }
+    }
+
+    /**
+     * Repeatedly loads and unloads a nanoapp asynchronously, and verifies that the naonapp is
+     * loaded successfully.
+     */
+    public void loadUnloadAsyncTest() throws Exception {
+        ContextHubTransaction<Void> transaction;
+        for (int i = 0; i < NUM_TEST_CYCLES; i++) {
+            transaction = mTestHelper.loadNanoApp(mNanoAppBinary);
+            waitForCompleteAsync(transaction);
+            mTestHelper.assertNanoAppsLoaded(mNanoAppIdList);
+
+            transaction = mTestHelper.unloadNanoApp(mNanoAppBinary.getNanoAppId());
+            waitForCompleteAsync(transaction);
+            mTestHelper.assertNanoAppsLoaded(Collections.emptyList());
+        }
+    }
+
+    private void waitForCompleteAsync(ContextHubTransaction<Void> transaction)
+            throws InterruptedException {
+        OnLoadUnloadCompleteListener listener = new OnLoadUnloadCompleteListener();
+        transaction.setOnCompleteListener(listener);
+        String type =
+                ContextHubTransaction.typeToString(transaction.getType(), /* upperCase= */ false);
+        long timeoutThreshold =
+                (transaction.getType() == ContextHubTransaction.TYPE_LOAD_NANOAPP)
+                        ? TIMEOUT_SECONDS_LOAD
+                        : TIMEOUT_SECONDS_UNLOAD;
+        boolean isCountedDown =
+                listener.getCountDownLatch().await(timeoutThreshold, TimeUnit.SECONDS);
+        assertWithMessage(
+                        "Waiting for %s transaction timeout after %s seconds",
+                        type, timeoutThreshold)
+                .that(isCountedDown)
+                .isTrue();
+    }
+}
