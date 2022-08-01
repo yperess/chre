00fdaf350d71782783ca9c67d0b42ac366e84806
karthik bharadwaj <karthikmb@google.com>
2022-08-01 11:25:10 -0700

    Add Clang support for Cortex-M builds
    Add a new makefile to define the Clang toolchain path to enable
    all targets to make use of it. Update the X86 target to use the
    new defs, and the Cortex-M target to incorporate Clang support.
    
    Bug: 240981468
    Test: Compile.
    Change-Id: I58ba86b65afb0b68c8496764b6a6f25c363e70cd

--- build/arch/cortexm.mk
+++ build/arch/cortexm.mk
@@ -7,16 +7,22 @@
 # If building for the Cortex-M target, ensure that the user has specified a path
 # to the Cortex-M toolchain that they wish to use.
 ifeq ($(CORTEXM_TOOLS_PREFIX),)
-$(error "You must supply a CORTEXM_TOOLS_PREFIX environment variable \
-         containing a path to the Cortex-M toolchain. Example: \
-         export CORTEXM_TOOLS_PREFIX=$$HOME/bin/gcc-arm-none-eabi-5_3-2016q1")
-endif
+# Try to default to Android's Clang prebuilts if the tools prefix isn't
+# specified.
+include $(CHRE_PREFIX)/build/clang.mk
+endif # CORTEXM_TOOLS_PREFIX
 
 # Cortex-M Tools ###############################################################
 
+ifeq ($(IS_CLANG_TOOLCHAIN),)
 TARGET_AR = $(CORTEXM_TOOLS_PREFIX)/bin/arm-none-eabi-ar
 TARGET_CC = $(CORTEXM_TOOLS_PREFIX)/bin/arm-none-eabi-g++
 TARGET_LD = $(CORTEXM_TOOLS_PREFIX)/bin/arm-none-eabi-ld
+else
+TARGET_AR = $(CLANG_TOOLCHAIN_PATH)/bin/llvm-ar
+TARGET_CC = $(CLANG_TOOLCHAIN_PATH)/bin/clang
+TARGET_LD = $(CLANG_TOOLCHAIN_PATH)/bin/ld.lld
+endif
 
 # Cortex-M Compiler Flags ######################################################
 
@@ -24,11 +30,23 @@ TARGET_LD = $(CORTEXM_TOOLS_PREFIX)/bin/arm-none-eabi-ld
 TARGET_CFLAGS += $(CORTEXM_CFLAGS)
 
 # Code generation flags.
-TARGET_CFLAGS += -mthumb
-TARGET_CFLAGS += -mfloat-abi=softfp
-TARGET_CFLAGS += -mno-thumb-interwork
-TARGET_CFLAGS += -ffast-math
-TARGET_CFLAGS += -fsingle-precision-constant
+#TODO(b/240981468): Float ABI soft -> hard pending CMSIS upgrade.
+CLANG_CFLAGS += -mfloat-abi=soft
+GCC_CFLAGS += -mthumb
+GCC_CFLAGS += -mfloat-abi=softfp
+GCC_CFLAGS += -mno-thumb-interwork
+GCC_CFLAGS += -ffast-math
+GCC_CFLAGS += -fsingle-precision-constant
+
+TARGET_CFLAGS += -ffunction-sections
+TARGET_CFLAGS += -fdata-sections
+TARGET_CFLAGS += -fshort-enums
+TARGET_CFLAGS += -fno-unwind-tables
+TARGET_CFLAGS += -mabi=aapcs
+
+ifneq ($(IS_NANOAPP_BUILD),)
+TARGET_CFLAGS += -fpic
+endif
 
 ifeq ($(IS_ARCHIVE_ONLY_BUILD), true)
 COMMON_CXX_CFLAGS += -fno-use-cxa-atexit
@@ -59,7 +77,15 @@ endif
 # Set the Cortex-M architecture.
 ifeq ($(CORTEXM_ARCH), m4)
 TARGET_CFLAGS += -mcpu=cortex-m4
-TARGET_CFLAGS += -mfpu=fpv4-sp-d16
+CLANG_CFLAGS += --target=arm-none-eabi
+# TODO(b/240981468): Make the FPU flag generic after CMSIS upgrade.
+GCC_CFLAGS += -mfpu=fpv4-sp-d16
+endif
+
+ifeq ($(IS_CLANG_TOOLCHAIN),)
+TARGET_CFLAGS += $(GCC_CFLAGS)
+else
+TARGET_CFLAGS += $(CLANG_CFLAGS)
 endif
 
 # Optimization Level ###########################################################
