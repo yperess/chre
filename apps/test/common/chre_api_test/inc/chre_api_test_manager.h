139c87fe8b8ed32dcafa550125d3620ec2786afb
Matthew Sedam <matthewsedam@google.com>
2022-12-12 15:52:21 +0000

    Add CHRE API Test nanoapp to test nanoapps
    This nanoapp will expose CHRE APIs through Pigweed RPC to simplify
    testing.
    
    Bug: 258074235
    Test: Build the nanoapp
    Change-Id: I171bd4fad601afb98459cb23697d10792424ae35

---
+++ apps/test/common/chre_api_test/inc/chre_api_test_manager.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CHRE_API_TEST_MANAGER_H_
+#define CHRE_API_TEST_MANAGER_H_
+
+#include <chre.h>
+#include <cstdint>
+
+#include "chre/re.h"
+#include "chre/util/pigweed/rpc_server.h"
+#include "chre/util/singleton.h"
+#include "rpc/chre_api_test.rpc.pb.h"
+
+class ChreApiTestService final
+    : public chre::rpc::pw_rpc::nanopb::ChreApiTestService::Service<
+          ChreApiTestService> {};
+
+/**
+ * Handles RPC requests for the CHRE API Test nanoapp
+ */
+class ChreApiTestManager {
+ public:
+  /**
+   * Allows the manager to do any init necessary as part of nanoappStart.
+   */
+  bool start();
+
+  /**
+   * Handle a CHRE event.
+   *
+   * @param senderInstanceId    the instand ID that sent the event.
+   * @param eventType           the type of the event.
+   * @param eventData           the data for the event.
+   */
+  void handleEvent(uint32_t senderInstanceId, uint16_t eventType,
+                   const void *eventData);
+
+  /**
+   * Allows the manager to do any cleanup necessary as part of nanoappEnd.
+   */
+  void end();
+
+  /**
+   * Sets the permission for the next server message.
+   *
+   * @params permission Bitmasked CHRE_MESSAGE_PERMISSION_.
+   */
+  void setPermissionForNextMessage(uint32_t permission);
+
+ private:
+  // RPC server
+  chre::RpcServer mServer;
+
+  // pw_rpc service used to process the RPCs.
+  ChreApiTestService mChreApiTestService;
+};
+
+typedef chre::Singleton<ChreApiTestManager> ChreApiTestManagerSingleton;
+
+#endif  // CHRE_API_TEST_MANAGER_H_
---
+++ apps/test/common/chre_api_test/src/chre_api_test_manager.cc
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "chre_api_test_manager.h"
+
+#include <limits>
+
+#include "chre.h"
+#include "chre/util/nanoapp/log.h"
+
+bool ChreApiTestManager::start() {
+  chre::RpcServer::Service service = {.service = mChreApiTestService,
+                                      .id = 0x61002d392de8430a,
+                                      .version = 0x01000000};
+  if (!mServer.registerServices(1, &service)) {
+    LOGE("Error while registering the service");
+    return false;
+  }
+
+  return true;
+}
+
+void ChreApiTestManager::setPermissionForNextMessage(uint32_t permission) {
+  mServer.setPermissionForNextMessage(permission);
+}
+
+void ChreApiTestManager::handleEvent(uint32_t senderInstanceId,
+                                     uint16_t eventType,
+                                     const void *eventData) {
+  if (!mServer.handleEvent(senderInstanceId, eventType, eventData)) {
+    LOGE("An RPC error occurred");
+  }
+}
+
+void ChreApiTestManager::end() {
+  // do nothing
+}
