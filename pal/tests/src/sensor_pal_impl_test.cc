6b3b07ff6981c5314f880f3f23b87d9e2c901988
Matthew Sedam <matthewsedam@google.com>
2023-01-20 20:25:38 +0000

    Initialize and deinit chre::TaskManagerSingleton in the sensor PAL implementation test
    Bug: 246316711
    Test: ./run_pal_impl_tests.sh
    Test: ./run_tests.sh
    Test: ./run_sim.sh
    Test: run chre_unit_tests
    Test: run chre_simulation_tests
    Change-Id: I16b200c80532c1a7b9ac90c76088d27216c0e7ff

--- pal/tests/src/sensor_pal_impl_test.cc
+++ pal/tests/src/sensor_pal_impl_test.cc
@@ -18,6 +18,7 @@
 
 #include "chre/pal/sensor.h"
 #include "chre/platform/condition_variable.h"
+#include "chre/platform/linux/task_util/task_manager.h"
 #include "chre/platform/mutex.h"
 #include "chre/platform/shared/pal_system_api.h"
 #include "chre/util/fixed_size_vector.h"
@@ -124,6 +125,7 @@ void flushCompleteCallback(uint32_t sensorInfoIndex, uint32_t flushRequestId,
 class PalSensorTest : public testing::Test {
  protected:
   void SetUp() override {
+    chre::TaskManagerSingleton::init();
     gCallbacks = MakeUnique<Callbacks>();
     mApi = chrePalSensorGetApi(CHRE_PAL_SENSOR_API_CURRENT_VERSION);
     ASSERT_NE(mApi, nullptr);
@@ -136,6 +138,7 @@ class PalSensorTest : public testing::Test {
     if (mApi != nullptr) {
       mApi->close();
     }
+    chre::TaskManagerSingleton::deinit();
   }
 
   //! CHRE PAL implementation API.
