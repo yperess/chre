2b16a6495ce58dcccd579323d0074b47e4c94931
Bryan Lavrich <lavrich@google.com>
2023-01-26 22:20:09 -0800

    Use hermetic python for pw_tokenizer
    Fixes builds on build server with old python version.
    
    Bug: 264604977
    Test: Verify python invocation with VERBOSE=1 build
    Change-Id: Ic114b4c615283d67fa921e495236d3343ebd5872

--- external/pigweed/pw_tokenizer.mk
+++ external/pigweed/pw_tokenizer.mk
@@ -15,10 +15,10 @@ PIGWEED_TOKENIZER_DIR=$(ANDROID_BUILD_TOP)/external/pigweed
 PIGWEED_SCRIPTS_DIR=$(PIGWEED_TOKENIZER_DIR)/pw_tokenizer/py/pw_tokenizer
 
 # Variables used by build_template.mk to generate the token mapping
-TOKEN_MAP_GEN_CMD = $(PIGWEED_SCRIPTS_DIR)/database.py create --force \
-                    --type binary --database
-TOKEN_MAP_CSV_GEN_CMD = $(PIGWEED_SCRIPTS_DIR)/database.py create --force \
-                    --type csv --database
+TOKEN_MAP_GEN_CMD     = $(PYTHON) $(PIGWEED_SCRIPTS_DIR)/database.py create \
+                          --force --type binary --database
+TOKEN_MAP_CSV_GEN_CMD = $(PYTHON) $(PIGWEED_SCRIPTS_DIR)/database.py create \
+                          --force --type csv --database
 
 # Pigweed source files
 COMMON_SRCS += $(PIGWEED_DIR)/pw_tokenizer/encode_args.cc
