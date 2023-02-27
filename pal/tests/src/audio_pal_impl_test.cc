a525ec44cabcbd3879a348a54f2c3b1aa1826de4
Matthew Sedam <matthewsedam@google.com>
2023-02-27 20:57:26 +0000

    Increase timeout for PalAudioTest.GetDataEvent to 25 ms
    This CL increases the timeout for getting the audio data
    to 25 ms, which is necessary to reduce flakiness when the
    TaskManager has high usage.
    
    This CL also fixes the address sanitizer errors.
    
    Bug: 264725561
    Test: Run chre_unit_tests in presubmit
    Change-Id: I862e7f76f880bafc9c6bff543cdb0186f2dccc0f

--- pal/tests/src/audio_pal_impl_test.cc
+++ pal/tests/src/audio_pal_impl_test.cc
@@ -126,8 +126,8 @@ TEST_F(PalAudioTest, GetDataEvent) {
 
   LockGuard<Mutex> lock(gCallbacks->mMutex);
   gCallbacks->mCondVarDataEvents.wait_for(
-      gCallbacks->mMutex, Nanoseconds(10 * kOneMillisecondInNanoseconds));
-  EXPECT_TRUE(gCallbacks->mDataEvent.has_value());
+      gCallbacks->mMutex, Nanoseconds(25 * kOneMillisecondInNanoseconds));
+  ASSERT_TRUE(gCallbacks->mDataEvent.has_value());
   struct chreAudioDataEvent *event = gCallbacks->mDataEvent.value();
   EXPECT_EQ(event->handle, 0);
   EXPECT_EQ(event->sampleCount, 1000);
