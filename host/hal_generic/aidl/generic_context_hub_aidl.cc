31ea9ed4d7798ceb6504bda2b9486ae82e18dc68
Matthew Sedam <matthewsedam@google.com>
2023-03-06 18:31:37 +0000

    Pass the context hub ID to getPreloadedNanoappIds
    Bug: 271846335
    Test: m, flash, boot # normal
    Test: logs # normal
    Test: Dump context hub service and AIDL HAL # normal
    Test: atest VtsAidlHalContextHubTargetTest
    Change-Id: I3df4f261a3d22858b53a7e7ba357691227a8bd38

--- host/hal_generic/aidl/generic_context_hub_aidl.cc
+++ host/hal_generic/aidl/generic_context_hub_aidl.cc
@@ -222,7 +222,12 @@ ScopedAStatus ContextHub::queryNanoapps(int32_t contextHubId) {
 }
 
 ::ndk::ScopedAStatus ContextHub::getPreloadedNanoappIds(
-    std::vector<int64_t> *out_preloadedNanoappIds) {
+    int32_t contextHubId, std::vector<int64_t> *out_preloadedNanoappIds) {
+  if (contextHubId != kDefaultHubId) {
+    LOGE("Invalid ID %" PRId32, contextHubId);
+    return ScopedAStatus::fromExceptionCode(EX_ILLEGAL_ARGUMENT);
+  }
+
   if (out_preloadedNanoappIds == nullptr) {
     return ScopedAStatus::fromExceptionCode(EX_ILLEGAL_ARGUMENT);
   }
@@ -502,7 +507,8 @@ ScopedAStatus ContextHub::enableTestMode() {
     LOGE("There exists a pending load transaction. Cannot enable test mode.");
   } else if (!queryNanoappsInternal(kDefaultHubId, &loadedNanoappIds)) {
     LOGE("Could not query nanoapps to enable test mode.");
-  } else if (!getPreloadedNanoappIds(&preloadedNanoappIds).isOk()) {
+  } else if (!getPreloadedNanoappIds(kDefaultHubId, &preloadedNanoappIds)
+                  .isOk()) {
     LOGE("Unable to get preloaded nanoapp IDs from the config file.");
   } else {
     std::sort(loadedNanoappIds.begin(), loadedNanoappIds.end());
