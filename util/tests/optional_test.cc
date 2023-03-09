997ca3e89779ec10698ba50777d055d4c8d484aa
Yixuan Wang <yixuanwang@google.com>
2023-03-09 21:51:52 +0000

    Revert "Add nullopt support for chre::Optional"
    This reverts commit 96745720e4479bad2b7f682467459b50b3183bad.
    
    Reason for revert: broke presubmit
    
    Change-Id: I6f6421c4619f078e5258ca7e9bf46106fd1fece7

--- util/tests/optional_test.cc
+++ util/tests/optional_test.cc
@@ -15,7 +15,6 @@
  * limitations under the License.
  */
 
-#include <optional>
 #include "gtest/gtest.h"
 
 #include "chre/util/non_copyable.h"
@@ -51,13 +50,6 @@ TEST(Optional, ShouldDestructContainedObject) {
 TEST(Optional, NoValueByDefault) {
   Optional<int> myInt;
   EXPECT_FALSE(myInt.has_value());
-}
-
-TEST(Optional, nullopt) {
-  Optional<int> opt1 = chre::nullopt;
-  EXPECT_FALSE(opt1.has_value());
-  Optional<int> opt2 = std::nullopt;
-  EXPECT_FALSE(opt2.has_value());
 }
 
 TEST(Optional, NonDefaultValueByDefault) {
