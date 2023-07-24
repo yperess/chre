5ebea9b4bdcf624167f1f2b40db9812c1eb56a31
Matthew Sedam <matthewsedam@google.com>
2023-07-24 23:36:33 +0000

    Use a one-shot Task in the linux audio PAL
    This CL uses a one-shot Task in the linux audio PAL to fix
    issues with test flakiness related to a race condition.
    
    Bug: 292121852
    Test: stress -c 128 # num CPUs
    Test: Run AudioCanSubscribeAndUnsubscribeToDataEvents 10^4 times with no failures
    Change-Id: I9bd78674544188d0803a56b3736c5450d38186f2

--- platform/linux/pal_audio.cc
+++ platform/linux/pal_audio.cc
@@ -80,9 +80,6 @@ void sendHandle0Events(uint32_t numSamples) {
       static_cast<const uint8_t *>(chre::memoryAlloc(numSamples));
 
   gCallbacks->audioDataEventCallback(data.release());
-
-  // Cancel the task so this is only run once with a delay.
-  TaskManagerSingleton::get()->cancelTask(gHandle0TaskId.value());
 }
 
 bool chrePalAudioApiRequestAudioDataEvent(uint32_t handle, uint32_t numSamples,
@@ -96,7 +93,7 @@ bool chrePalAudioApiRequestAudioDataEvent(uint32_t handle, uint32_t numSamples,
     gIsHandle0Enabled = true;
     gHandle0TaskId = TaskManagerSingleton::get()->addTask(
         [numSamples]() { sendHandle0Events(numSamples); },
-        std::chrono::nanoseconds(eventDelayNs));
+        std::chrono::nanoseconds(eventDelayNs), true /*isOneShot*/);
     if (!gHandle0TaskId.has_value()) {
       return false;
     }
