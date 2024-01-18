3441490dbf534ea63f73ff83d206e55cd452d1e2
Chia-Chi Teng <ccteng@google.com>
2024-01-18 23:13:00 +0000

    ChreCrossValidatorWifi add debug information
    Bug: 185188753
    Test: presubmit
    Change-Id: I902bc2e4706c7a807ba06c8601cc80fe34274136

--- apps/test/common/chre_cross_validator_wifi/src/chre_cross_validator_wifi_manager.cc
+++ apps/test/common/chre_cross_validator_wifi/src/chre_cross_validator_wifi_manager.cc
@@ -169,17 +169,22 @@ void Manager::compareAndSendResultToHost() {
                            (mApScanResultsSize != mChreScanResultsSize);
   bool aboveMaxSizeCheck = (mApScanResultsSize > mMaxChreResultSize) &&
                            (mApScanResultsSize < mChreScanResultsSize);
-  // TODO(b/185188753): Log info about all scan results so that it is easier
-  // to figure out which AP or CHRE scan results are missing or corrupted.
+
   if (belowMaxSizeCheck || aboveMaxSizeCheck) {
+    LOGE("AP and CHRE wifi scan result counts differ, AP = %" PRIu8
+         ", CHRE = %" PRIu8 ", MAX = %" PRIu8,
+         mApScanResultsSize, mChreScanResultsSize, mMaxChreResultSize);
+    for (uint16_t i = 0; i < mChreScanResultsSize; i++) {
+      LOGE("CHRE[%u]: %s", i, mChreScanResults[i].getSsid());
+    }
+    for (uint16_t i = 0; i < mApScanResultsSize; i++) {
+      LOGE("AP[%u]: %s", i, mApScanResults[i].getSsid());
+    }
     test_shared::sendTestResultWithMsgToHost(
         mCrossValidatorState.hostEndpoint,
         chre_cross_validation_wifi_MessageType_STEP_RESULT, false /*success*/,
         "There is a different number of AP and CHRE scan results.",
         false /*abortOnFailure*/);
-    LOGE("AP and CHRE wifi scan result counts differ, AP = %" PRIu8
-         ", CHRE = %" PRIu8,
-         mApScanResultsSize, mChreScanResultsSize);
 
     return;
   }
