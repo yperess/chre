ab8d3de8a6a995268e9db2d9a1781ad02c169a42
William Escande <wescande@google.com>
2024-06-28 01:35:32 +0000

    bleScanningTest: handle all initial bluetooth state
    The test didn't handle properly a Bluetooth already disabled and OFF
    while the scanning was enabled (but not being made use of).
    
    The fundamental behavior of the test hasn't change, we require all the
    combination of Bluetooth and ScanningMode, and assert that the chre is
    enabled when expected
    
    Also making some simplification:
    * Using truth for prettier assert message
    * Using mock for the receiver to inline waiting on the intent
    * Only listen to BLE_STATE_CHANGED as this contains everything needed
    * Move some variable to static as they are never changing
    * Apply android formatting
    * Add some logs to be able to identify what happened in case of failure
    
    Bug: 349684439
    Test: Presubmit
    Change-Id: Idfd5a681194aca289e0c8351307e9f63538ca6a6

--- java/test/settings/src/com/google/android/chre/test/setting/ContextHubBleSettingsTestExecutor.java
+++ java/test/settings/src/com/google/android/chre/test/setting/ContextHubBleSettingsTestExecutor.java
@@ -16,6 +16,16 @@
 
 package com.google.android.chre.test.setting;
 
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+
 import android.app.Instrumentation;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
@@ -29,156 +39,156 @@ import android.util.Log;
 import com.google.android.chre.nanoapp.proto.ChreSettingsTest;
 import com.google.android.utils.chre.SettingsUtil;
 
-import org.junit.Assert;
+import org.hamcrest.Matcher;
+import org.hamcrest.core.AllOf;
+import org.mockito.hamcrest.MockitoHamcrest;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
-/**
- * A test to check for behavior when Bluetooth settings are changed.
- */
+/** A test to check for behavior when Bluetooth settings are changed. */
 public class ContextHubBleSettingsTestExecutor {
     private static final String TAG = "ContextHubBleSettingsTestExecutor";
-    private final ContextHubSettingsTestExecutor mExecutor;
 
-    private final Instrumentation mInstrumentation =
+    private static final Instrumentation sInstrumentation =
             androidx.test.platform.app.InstrumentationRegistry.getInstrumentation();
 
-    private final Context mContext = mInstrumentation.getTargetContext();
+    private static final Context sContext = sInstrumentation.getTargetContext();
 
-    private final SettingsUtil mSettingsUtil;
+    private static final SettingsUtil sSettingsUtil = new SettingsUtil(sContext);
+
+    private static final BluetoothAdapter sAdapter =
+            sContext.getSystemService(BluetoothManager.class).getAdapter();
+
+    private final ContextHubSettingsTestExecutor mExecutor;
 
     private boolean mInitialBluetoothEnabled;
-
+    private boolean mInitialScanningEnabled;
     private boolean mInitialAirplaneMode;
-
-    private boolean mInitialBluetoothScanningEnabled;
-
-    public static class BluetoothUpdateListener {
-        public BluetoothUpdateListener(int state) {
-            mExpectedState = state;
-        }
-
-        // Expected state of the BT Adapter
-        private final int mExpectedState;
-
-        public CountDownLatch mBluetoothLatch = new CountDownLatch(1);
-
-        public BroadcastReceiver mBluetoothUpdateReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(intent.getAction())
-                        || BluetoothAdapter.ACTION_BLE_STATE_CHANGED.equals(
-                                intent.getAction())) {
-                    if (mExpectedState == intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1)) {
-                        mBluetoothLatch.countDown();
-                    }
-                }
-            }
-        };
-    }
 
     public ContextHubBleSettingsTestExecutor(NanoAppBinary binary) {
         mExecutor = new ContextHubSettingsTestExecutor(binary);
-        mSettingsUtil = new SettingsUtil(mContext);
     }
 
-    /**
-     * Should be called in a @Before method.
-     */
+    /** Should be called in a @Before method. */
     public void setUp() throws InterruptedException {
-        mInitialBluetoothEnabled = mSettingsUtil.isBluetoothEnabled();
-        mInitialBluetoothScanningEnabled = mSettingsUtil.isBluetoothScanningAlwaysEnabled();
-        mInitialAirplaneMode = mSettingsUtil.isAirplaneModeOn();
-        Log.d(TAG, "isBluetoothEnabled=" + mInitialBluetoothEnabled
-                    + "; isBluetoothScanningEnabled=" + mInitialBluetoothScanningEnabled
-                    + "; isAirplaneModeOn=" + mInitialAirplaneMode);
-        mSettingsUtil.setAirplaneMode(false /* enable */);
+        mInitialBluetoothEnabled = sSettingsUtil.isBluetoothEnabled();
+        mInitialScanningEnabled = sSettingsUtil.isBluetoothScanningAlwaysEnabled();
+        mInitialAirplaneMode = sSettingsUtil.isAirplaneModeOn();
+        Log.d(
+                TAG,
+                ("isBluetoothEnabled=" + mInitialBluetoothEnabled)
+                        + (" isBluetoothScanningEnabled=" + mInitialScanningEnabled)
+                        + (" isAirplaneModeOn=" + mInitialAirplaneMode));
+        sSettingsUtil.setAirplaneMode(false /* enable */);
         mExecutor.init();
     }
 
     public void runBleScanningTest() throws InterruptedException {
-        runBleScanningTest(false /* enableBluetooth */, false /* enableBluetoothScanning */);
-        runBleScanningTest(true /* enableBluetooth */, false /* enableBluetoothScanning */);
-        runBleScanningTest(false /* enableBluetooth */, true /* enableBluetoothScanning */);
-        runBleScanningTest(true /* enableBluetooth */, true /* enableBluetoothScanning */);
+        runBleScanningTest(false /* enableBluetooth */, false /* enableScanning */);
+        runBleScanningTest(true /* enableBluetooth */, false /* enableScanning */);
+        runBleScanningTest(false /* enableBluetooth */, true /* enableScanning */);
+        runBleScanningTest(true /* enableBluetooth */, true /* enableScanning */);
     }
 
-    /**
-     * Should be called in an @After method.
-     */
+    /** Should be called in an @After method. */
     public void tearDown() throws InterruptedException {
         mExecutor.deinit();
-        mSettingsUtil.setBluetooth(mInitialBluetoothEnabled);
-        mSettingsUtil.setBluetoothScanningSettings(mInitialBluetoothScanningEnabled);
-        mSettingsUtil.setAirplaneMode(mInitialAirplaneMode);
+        sSettingsUtil.setBluetooth(mInitialBluetoothEnabled);
+        sSettingsUtil.setBluetoothScanningSettings(mInitialScanningEnabled);
+        sSettingsUtil.setAirplaneMode(mInitialAirplaneMode);
     }
 
-    /**
-     * Sets the BLE scanning settings on the device.
-     * @param enable                    true to enable Bluetooth settings, false to disable it.
-     * @param enableBluetoothScanning   if true, enable BLE scanning; false, otherwise
-     */
-    private void setBluetoothSettings(boolean enable, boolean enableBluetoothScanning) {
-        // Check if already in the desired state
-        if ((enable == mSettingsUtil.isBluetoothEnabled())
-                 && (enableBluetoothScanning == mSettingsUtil.isBluetoothScanningAlwaysEnabled())) {
+    @SafeVarargs
+    private void verifyIntentReceived(BroadcastReceiver receiver, Matcher<Intent>... matchers) {
+        verify(receiver, timeout(10_000))
+                .onReceive(any(Context.class), MockitoHamcrest.argThat(AllOf.allOf(matchers)));
+    }
+
+    private int getBluetoothState() {
+        if (sAdapter.getState() == BluetoothAdapter.STATE_ON) {
+            return BluetoothAdapter.STATE_ON;
+        } else if (sAdapter.isLeEnabled()) {
+            return BluetoothAdapter.STATE_BLE_ON;
+        } else {
+            return BluetoothAdapter.STATE_OFF;
+        }
+    }
+
+    /** return true if a state change occurred */
+    private void setBluetoothMode(int wantedState) {
+        BroadcastReceiver receiver = mock(BroadcastReceiver.class);
+        sContext.registerReceiver(
+                receiver, new IntentFilter(BluetoothAdapter.ACTION_BLE_STATE_CHANGED));
+        try {
+            if (wantedState == getBluetoothState()) {
+                Log.d(TAG, "Bluetooth already in " + BluetoothAdapter.nameForState(wantedState));
+                return;
+            }
+
+            switch (wantedState) {
+                case BluetoothAdapter.STATE_ON -> {
+                    sSettingsUtil.setBluetooth(true);
+                }
+                case BluetoothAdapter.STATE_BLE_ON -> {
+                    // When Bluetooth is ON, calling enableBLE will not do anything on its own. We
+                    // also need to disable the classic Bluetooth
+                    assertThat(sAdapter.enableBLE()).isTrue();
+                    sSettingsUtil.setBluetooth(false);
+                }
+                case BluetoothAdapter.STATE_OFF -> {
+                    sSettingsUtil.setBluetooth(false);
+                }
+            }
+
+            verifyIntentReceived(receiver, hasExtra(BluetoothAdapter.EXTRA_STATE, wantedState));
+        } finally {
+            sContext.unregisterReceiver(receiver);
+        }
+    }
+
+    void setScanningMode(boolean enableScanning) {
+        if (enableScanning == sSettingsUtil.isBluetoothScanningAlwaysEnabled()) {
+            Log.d(TAG, "Scanning is already in the expected mode: " + enableScanning);
             return;
         }
 
-        int state = BluetoothAdapter.STATE_OFF;
-        if (enable) {
-            state = BluetoothAdapter.STATE_ON;
-        } else if (enableBluetoothScanning) {
-            state = BluetoothAdapter.STATE_BLE_ON;
-        }
-
-        BluetoothUpdateListener bluetoothUpdateListener = new BluetoothUpdateListener(state);
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
-        filter.addAction(BluetoothAdapter.ACTION_BLE_STATE_CHANGED);
-        mContext.registerReceiver(bluetoothUpdateListener.mBluetoothUpdateReceiver, filter);
-
-        mSettingsUtil.setBluetooth(enable);
-        mSettingsUtil.setBluetoothScanningSettings(enableBluetoothScanning);
-        if (!enable && enableBluetoothScanning) {
-            BluetoothManager bluetoothManager = mContext.getSystemService(BluetoothManager.class);
-            Assert.assertTrue(bluetoothManager != null);
-            BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
-            Assert.assertTrue(bluetoothAdapter != null);
-            Assert.assertTrue(bluetoothAdapter.enableBLE());
-        }
-        try {
-            boolean success = bluetoothUpdateListener.mBluetoothLatch.await(10, TimeUnit.SECONDS);
-            Assert.assertTrue("Timeout waiting for signal: bluetooth update listener", success);
-            Assert.assertTrue(enable == mSettingsUtil.isBluetoothEnabled());
-            Assert.assertTrue(enableBluetoothScanning
-                    == mSettingsUtil.isBluetoothScanningAlwaysEnabled());
-
-            // Wait a few seconds to ensure setting is propagated to CHRE path
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            Assert.fail(e.getMessage());
-        }
-
-        mContext.unregisterReceiver(bluetoothUpdateListener.mBluetoothUpdateReceiver);
+        Log.d(TAG, "Setting scanning into: " + enableScanning);
+        sSettingsUtil.setBluetoothScanningSettings(enableScanning);
     }
 
     /**
      * Helper function to run the test
      *
-     * @param enableBluetooth         if bluetooth is enabled
-     * @param enableBluetoothScanning if bluetooth scanning is always enabled
+     * @param enableBluetooth if bluetooth is enabled
+     * @param enableScanning if bluetooth scanning is always enabled
      */
-    private void runBleScanningTest(boolean enableBluetooth,
-            boolean enableBluetoothScanning) throws InterruptedException {
-        setBluetoothSettings(enableBluetooth, enableBluetoothScanning);
+    private void runBleScanningTest(boolean enableBluetooth, boolean enableScanning)
+            throws InterruptedException {
+        Log.d(TAG, "runTest: Bluetooth=" + enableBluetooth + " Scanning=" + enableScanning);
+        setScanningMode(enableScanning);
 
-        boolean enableFeature = enableBluetooth || enableBluetoothScanning;
-        ChreSettingsTest.TestCommand.State state = enableFeature
-                ? ChreSettingsTest.TestCommand.State.ENABLED
-                : ChreSettingsTest.TestCommand.State.DISABLED;
-        mExecutor.startTestAssertSuccess(
-                ChreSettingsTest.TestCommand.Feature.BLE_SCANNING, state);
+        if (enableBluetooth) {
+            setBluetoothMode(BluetoothAdapter.STATE_ON);
+        } else if (enableScanning) {
+            // If scanning just get toggle ON, it may take times to propagate and to allow BLE_ON…
+            setBluetoothMode(BluetoothAdapter.STATE_BLE_ON);
+        } else {
+            setBluetoothMode(BluetoothAdapter.STATE_OFF);
+        }
+
+        try {
+            // Wait to ensure settings are propagated to CHRE path
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            assertWithMessage(e.getMessage()).fail();
+        }
+
+        assertThat(sSettingsUtil.isBluetoothEnabled()).isEqualTo(enableBluetooth);
+        assertThat(sSettingsUtil.isBluetoothScanningAlwaysEnabled()).isEqualTo(enableScanning);
+
+        boolean enableFeature = enableBluetooth || enableScanning;
+        ChreSettingsTest.TestCommand.State state =
+                enableFeature
+                        ? ChreSettingsTest.TestCommand.State.ENABLED
+                        : ChreSettingsTest.TestCommand.State.DISABLED;
+        mExecutor.startTestAssertSuccess(ChreSettingsTest.TestCommand.Feature.BLE_SCANNING, state);
     }
 }
